f333aeedc911c09216c488df47276a9474498f2e
Cola <1020151695@qq.com>
2023-09-24 18:31:31 +0800

    update

--- array/1.py
+++ array/1.py
@@ -2,12 +2,9 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         idx = dict()
+
         for i in range(len(nums)):
             x = target-nums[i]
             if x in idx:
                 return [i, idx[x]]
             idx[x] = i
-
-print(Solution().twoSum(nums = [3,3], target = 6))
-
-
--- dfs/63.py
+++ dfs/63.py
@@ -5,20 +5,20 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         def dfs(row, column):
             if row<0 or row>=n or column<0 or column>=m \
-                    or obstacleGrid[row][column]==1 or count[row][column]==0:
+                    or obstacleGrid[row][column]==1 or record[row][column]==0:
                 return 0
 
             if row==n-1 and column==m-1:
                 return 1
-            if count[row][column]>=1:
-                return count[row][column]
+            if record[row][column]>=1:
+                return record[row][column]
 
-            count[row][column] = dfs(row+1, column)+dfs(row, column+1)
-            return count[row][column]
+            record[row][column] = dfs(row+1, column)+dfs(row, column+1)
+            return record[row][column]
 
         n = len(obstacleGrid)
         m = len(obstacleGrid[0])
-        count = [[-1]*m for _ in range(n)]  # 剪枝
+        record = [[-1]*m for _ in range(n)]
         return dfs(0, 0)
 
 
